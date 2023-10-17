cd6735d98ede9bb087686476fe80c3eb40ec276a
Wakio37 <your_email@example.com>
2023-10-17 17:45:16 +0000

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word)
+        
+        for potential_anagram in word_list:
+            if potential_anagram != self.word and sorted(potential_anagram) == sorted_word:
+                matches.append(potential_anagram)
+        
+        return matches
